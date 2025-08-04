27c5ffe3bf03e070299bab6affeca7a3302c8274
Yaseen <yaseen17861786@gmail.com>
2025-08-05 00:39:25 +0530

    added auth Service

---
+++ AuthServices/app/src/main/java/authservices/entities/UserRole.java
@@ -0,0 +1,23 @@
+package authservices.entities;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user_roles")
+
+
+public class UserRole {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "role_id")
+    private long roleId;
+    private String name;
+}
